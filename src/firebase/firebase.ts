aaef4e7f1f68451d6b2df4bb398a88ee91e600dc
Dimitre Ortt <dimiortt@gmail.com>
2022-05-21 18:25:51 -0300

    Removed garbage

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-// TODO: criar função firebaseConfig que simplesmente retorna a sua config
+// função firebaseConfig que simplesmente retorna a sua config
 import { firebaseConfig } from './firebaseConfig';
 
 export const app = initializeApp(firebaseConfig());
